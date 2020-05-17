b38255b3e167c012d3dc706ae2bc9fba97d262c5
Huseyin Oral <huseyin-545@hotmail.com>
2020-05-17 04:09:01 +0300

    guncelleme1

---
+++ BambistoreXamarin/BambistoreXamarin/Views/Anasayfa.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BambistoreXamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Anasayfa : ContentPage
+    {
+        public Anasayfa()
+        {
+            InitializeComponent();
+            List<string> slide = new List<string>()
+            {
+                "https://www.bambistore.com.tr/Uploads/Slider/kadin-yeni-sezon-ss20-209.jpg?t=20200512155012",
+                "https://www.bambistore.com.tr/Uploads/Slider/erkek-yeni-sezon-210.jpg?t=20200512155021"
+            };
+            CarouseSlide.ItemsSource = slide;
+        }
+    }
+}
